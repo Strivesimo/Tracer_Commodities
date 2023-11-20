bdad9d59a16229ead35c6eb88b1784c176f0151b
Strivesimo <simostrive@gmail.com>
2023-11-20 15:19:03 +0200

    Update tracercommodities.py
    ss

--- tracercommodities.py
+++ tracercommodities.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
-import plotly.express as px
+#import plotly.express as px
 
 st.set_page_config(page_title="Tracer Commodities Stock Status", layout = "wide")
 st.title(":blue[Tracer Commodities Stock Status]")
